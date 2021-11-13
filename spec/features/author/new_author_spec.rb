db6febe58e9dfca1c016b940d463111ed03fadbf
Ole Schlueter <ole@kyub.com>
2021-11-13 16:05:51 +0100

    Adds controoler and view for new authors

--- spec/features/author/new_author_spec.rb
+++ spec/features/author/new_author_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 describe "New author page", type: :feature do
   it "should exist at 'new_author_path' and render withour error" do
-    https://guides.rubyonrails.org/routing.html#path-and-url-helpers
+    # https://guides.rubyonrails.org/routing.html#path-and-url-helpers
     visit new_author_path
   end
 end
