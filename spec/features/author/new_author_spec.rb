88408bc9c30204bf3565652a75c685d16938e783
Ole Schlueter <ole@kyub.com>
2021-11-16 17:40:14 +0100

    Add test for displaying an error message when saving an invalid author

--- spec/features/author/new_author_spec.rb
+++ spec/features/author/new_author_spec.rb
@@ -21,4 +21,13 @@ describe "New author page", type: :feature do
     page.fill_in 'author[homepage]', with: 'http://wikipedia.org/Alan_Turing'
     find('input[type="submit"]').click
   end
+
+  it "should display an error when trying to save a invalid author" do
+    visit new_author_path
+    page.fill_in 'author[first_name]', with: 'Alan'
+    page.fill_in 'author[last_name]', with: ''
+    page.fill_in 'author[homepage]', with: 'http://wikipedia.org/Alan_Turing'
+    find('input[type="submit"]').click
+    expect(page).to have_text('error')
+  end
 end
