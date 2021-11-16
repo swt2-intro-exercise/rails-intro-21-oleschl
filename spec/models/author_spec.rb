16395df7c4ef40ff9a57e95a0d5df10a1e8dc873
Ole Schlueter <ole@kyub.com>
2021-11-16 13:32:01 +0100

    Add author models tests

--- spec/models/author_spec.rb
+++ spec/models/author_spec.rb
@@ -1,16 +1,20 @@
 require 'rails_helper'
 
-describe "Author", type :model do
+describe "Author", type: :model do
     it "initializes the first name correctly" do
-        author = Author.new("Alan", "Turing", "http://wikipedia.org/Alan_Turing")
+        author = Author.new({"first_name"=>"Alan", "last_name"=>"Turing", "homepage"=>"http://wikipedia.org/Alan_Turing"})
         expect(author.first_name).to eq("Alan")
     end
     it "initializes the last name correctly" do
-        author = Author.new("Alan", "Turing", "http://wikipedia.org/Alan_Turing")
+        author = Author.new({"first_name"=>"Alan", "last_name"=>"Turing", "homepage"=>"http://wikipedia.org/Alan_Turing"})
         expect(author.last_name).to eq("Turing")
     end
     it "initializes the homepage correctly" do
-        author = Author.new("Alan", "Turing", "http://wikipedia.org/Alan_Turing")
+        author = Author.new({"first_name"=>"Alan", "last_name"=>"Turing", "homepage"=>"http://wikipedia.org/Alan_Turing"})
         expect(author.homepage).to eq("http://wikipedia.org/Alan_Turing")
+    end
+    it "returns the correct full name" do
+        author = Author.new({"first_name"=>"Alan", "last_name"=>"Turing", "homepage"=>"http://wikipedia.org/Alan_Turing"})
+        expect(author.name).to eq("Alan Turing")
     end
 end
