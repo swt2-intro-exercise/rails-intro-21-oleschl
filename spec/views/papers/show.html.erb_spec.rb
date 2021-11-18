8413515b92f833996c232a40565f1fa4d2027585
Ole Schlueter <ole@kyub.com>
2021-11-18 15:47:36 +0100

    Failing test for #15

--- spec/views/papers/show.html.erb_spec.rb
+++ spec/views/papers/show.html.erb_spec.rb
@@ -15,4 +15,11 @@ RSpec.describe "papers/show", type: :view do
     expect(rendered).to match(/Venue/)
     expect(rendered).to match(/2/)
   end
+
+  it "should display the papers authors if there are any" do
+    @author = FactoryBot.create :author
+    @paper.authors = [@author]
+    render
+    expect(rendered).to have_text('Alan Turing')
+  end
 end
